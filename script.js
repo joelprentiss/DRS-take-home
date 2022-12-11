211530830664e793de46f1a2bd492961d862bff7
Joel Prentiss <joel.prentiss89@gmail.com>
2022-12-10 21:39:39 -0600

    finished dark/light button, finished css and html, filled in content with lorem ipsum

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+
+function changeCss(){
+    const css = document.getElementById('css-file');
+    const darkMode = document.getElementById('dark-mode');
+
+    if(darkMode.innerHTML == 'Dark Mode'){
+        css.setAttribute('href', 'darkmode.css');
+        darkMode.innerHTML = 'Light Mode';
+    } else{
+        css.setAttribute('href', 'main.css');
+        darkMode.innerHTML = 'Dark Mode';
+    }
+};
+
+document.getElementById('dark-mode').addEventListener('click', changeCss);
